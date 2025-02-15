55a54b01b9f9f18273f8ef45b4ccc8aec2b65b5d
Mazin98KKs <admin@certificatesco.com>
2025-02-15 17:32:22 +0400

    update

--- broadcast.js
+++ broadcast.js
@@ -1,6 +1,6 @@
 // Define environment variables directly in the code
-const WHATSAPP_API_URL = 'https://graph.facebook.com/v21.0/533978409805057/messages';
-const WHATSAPP_API_TOKEN = 'EAAIIvDZBBh5sBO5H9LMaa4iZAHqh8ab5a3jz1ZBaz0VbX0rjNS6ghEEDIEZA4txm6OuVpXcWYlVS65dQHj10ynNRxzZAppz0oGA6nBsmGlswRsKzK7kotSv65zd1nyKmWUj1XGrLjHUbB3bl8iUYkHr7Yxm9P0riETWHMwDxEqphZCsaqIedPXi6gtt1fZBaWhOkgZDZD';
+const WHATSAPP_API_URL = 'https://graph.facebook.com/v22.0/567838906410459/messages';
+const WHATSAPP_API_TOKEN = 'EAATw1es7w9gBO61z2q62nGzawL1mlGrFZAQLtrb7km2mZCnZAAengLuhDrddnRbalujZBdynPlnQImZCEsArVtUqKHXZAwTZBUk6it35vExomYfPZAMg3gOwqhDCnnUXnPZBsiiztjIZCiYhoKiOPW424EYtQRxHJfvkZCOfY70VyA9XD4IGbhVbF63OnPMVFAlvjWwEAZDZD';
 
 if (!WHATSAPP_API_URL || !WHATSAPP_API_TOKEN) {
   console.error('Missing required environment variables for WhatsApp API.');
@@ -65,7 +65,7 @@ async function sendBroadcast(templateName, recipientNumbers) {
     }
 
     console.log(`Sending messages to ${recipients.length} recipients...`);
-    await sendBroadcast('promo2', recipients);
+    await sendBroadcast('wel_sel', recipients);
     console.log('Broadcast complete.');
   } catch (error) {
     console.error('Error during broadcast process:', error.message);
