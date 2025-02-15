77cc7ec3c9558dc7f8ffa5969208c6a7ce1286fb
Mazin98KKs <admin@certificatesco.com>
2025-02-15 17:48:55 +0400

    update

--- broadcast.js
+++ broadcast.js
@@ -1,6 +1,6 @@
 // Define environment variables directly in the code
 const WHATSAPP_API_URL = 'https://graph.facebook.com/v22.0/567838906410459/messages';
-const WHATSAPP_API_TOKEN = 'EAATw1es7w9gBO61z2q62nGzawL1mlGrFZAQLtrb7km2mZCnZAAengLuhDrddnRbalujZBdynPlnQImZCEsArVtUqKHXZAwTZBUk6it35vExomYfPZAMg3gOwqhDCnnUXnPZBsiiztjIZCiYhoKiOPW424EYtQRxHJfvkZCOfY70VyA9XD4IGbhVbF63OnPMVFAlvjWwEAZDZD';
+const WHATSAPP_API_TOKEN = 'EAATw1es7w9gBO5PL0KZA0Mf0qZASrHUJ1oLCtBQGGZBzRW8YZCRWpfhqfO4SYcLRzvKF7c1cpNaFeZAdIozsOLTgXeKfBA2iln4jTqRrmnjATNyA2BNoeNi1yLuOBXINIXhXobjI4whnnZCk6Fw5ZBwm5vvgf55FRwzpzntRqwpI0mY98RhJGFgvFr2afJRInKvpAZDZD';
 
 if (!WHATSAPP_API_URL || !WHATSAPP_API_TOKEN) {
   console.error('Missing required environment variables for WhatsApp API.');
